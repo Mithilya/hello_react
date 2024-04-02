84cee9a992835fa15f2f45c757d879b97eff8d7c
Mithilya <113309804+Mithilya@users.noreply.github.com>
2024-04-01 20:38:49 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>Professor Siarto's Class</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Testing React for This Assignment
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
